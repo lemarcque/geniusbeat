4a25c1a87f9260f42320beff43dd8c02d1fa54ac
Henoc Sese <hello@capsulo.io>
2019-07-14 21:43:17 +0200

    preview page

--- app.js
+++ app.js
@@ -6,8 +6,7 @@
 	*********************************
 	*********************************
 	*********************************
-*/
-
+*/		
 
 /* auto resize */
 function textAreaAdjust(o) {
@@ -15,102 +14,37 @@ function textAreaAdjust(o) {
  	o.style.height = (25 + o.scrollHeight) + "px";
 }
 
-/* get DOM element */
-var view = {
-	'viewer': document.querySelector("#lyrics-viewer"),
-	'area': document.querySelector("#lyrics-textarea")
-}
-
-var btn = {
-	'edit': document.querySelector("#btn-edit"),
-	'cancel': document.querySelector("#btn-cancel"),
-	'record': document.querySelector("#btn-record"),
-	'play': document.querySelector("#btn-play")
-}
-
-/* click listener */
-btn['edit'].addEventListener("click", function (e) {
-	displayEditArea();
-});
-btn['cancel'].addEventListener("click", function (e) {
-	displayViewer();
-});
-btn['record'].addEventListener("click", function (e) {
-	if(isRecording == false) {
-		isRecording = true;
-		recordingSound();
-	}
-});
-
-btn['play'].addEventListener("click", function (e) {
-	if(isPlaying == false) {
-		isPlaying = true;
-		playSound();
-	}
-});
-
-var audio;
-
 function displayViewer() {
 	view['area'].style.display = "none";
 	btn['cancel'].style.display = "none";
-	btn['edit'].style.display = "block";
+	btn['edit'].style.display = "inline";
+	btn['record'].style.display = "inline";
+	btn['play'].style.display = "inline";
+	btn['save'].style.display = "none";
 	view['viewer'].style.display = "block";
-	//view['viewer'].innerHTML = lyrics;
 }
 
 function displayEditArea() {
 	view['viewer'].style.display = "none";
-	btn['cancel'].style.display = "block5";
+	btn['save'].style.display = "inline";
+	btn['cancel'].style.display = "inline";
 	btn['edit'].style.display = "none";
+	btn['record'].style.display = "none";
+	btn['play'].style.display = "none";
 	view['area'].style.display = "block";
 	view['area'].value = lyrics.replace(/<br *\/?>/gi, '\n');
 	textAreaAdjust(view['area']);
 }
 
-function recordingSound() {
-	if(audio != undefined) audio.pause();
-	audio = new Audio('vg-lagoh.mp3');
-	audio.pause();
-	audio.currentTime = 0;
+function updateLyrics() {
+	lyrics = view.area.value;
+	var data = lyrics.split("\n");
 
-	audio.ontimeupdate = function() {}
-	audio.onended = function() { console.log("FINISH"); recordedReferentTime = referentTime; }
-	audio.play();
-}
+	// remove all childs
+	while (view.viewer.firstChild) {
+	    view.viewer.firstChild.remove();
+	}
 
-function playSound() {
-	// reset settings
-	pointer = 0;
-
-	if(audio != undefined) audio.pause();
-	audio = new Audio('vg-lagoh.mp3');
-	audio.currentTime = 0;
-
-	audio.ontimeupdate = function() {
-   		if(Math.round(audio.currentTime) == Math.round(recordedReferentTime[pointerReferentTime])) {
-   			switchReferentPos(pointerReferentTime);
-   			pointerReferentTime++
-   		}
-	};
-	audio.play();
-} 
-
-
-document.addEventListener('keyup', logKey);
-
-function logKey(e) {
-	if(e.code == "Enter") nextReferent();
-}
-
-
-// TEMP
-var lyrics = "[Intro]<br>It's Vegedream<br>Atona tona oh<br>Naza eh lagoh ya oh<br>Naza eh lagoh ya oh<br>Naza eh<br>Atona tona oh<br>Naza eh lagoh ya oh<br><br>[Couplet unique]<br>J'ai charbonné tous les soirs<br>Ça a failli me tuer, oh<br>Je vivais un vrai cauchemar<br>Ça pouvait me tuer, oh<br>J'appelais maman tous les soirs<br>Ça a failli la tuer, oh<br>Et me voilà 7 ans plus tard<br>Je vais tuer, oh<br><br>[Refrain]<br>Ah comment je m’appelle?<br>Vegedream<br>Ah comment je m’appelle?<br>Vegedream<br>Ah comment je m’appelle?<br>Vegedream<br>Ah comment je m’appelle?<br>Ah Vegedream"
-
-var recordedReferentTime = [];
-
-function addLyrics() {
-	var data = lyrics.split("<br>");
 	data.forEach(function(element) {
 
 		if(element.length > 0) {
@@ -119,7 +53,7 @@ function addLyrics() {
 			span.innerHTML = element + "<br>"
 			
 			if(element.includes("[") && element.includes("]")) {
-				span.className = "separator"
+				span.className = "lyrics-separator"
 			}else {
 				span.className = "referent"
 			}
@@ -133,15 +67,45 @@ function addLyrics() {
 	referents = view.viewer.querySelectorAll('.referent')
 }
 
-var referents;
-var pointer = 0;
+function recordingSound() {
+	if(audio != undefined) audio.pause();
+	audio = new Audio("./" + fileName);
+	audio.pause();
+	audio.currentTime = 0;
+
+	audio.ontimeupdate = function() {}
+	audio.onended = function() { recordedReferentTime = referentTime; }
+	audio.play();
+}
+
+function playSound() {
+	if(audio != undefined) audio.pause();
+	audio = new Audio(fileName);
+	audio.currentTime = 0;
+	pointerReferentTime = 0;
+	pointer = 0;
+
+	audio.ontimeupdate = function() {
+   		if(Math.round(audio.currentTime) == Math.round(recordedReferentTime[pointerReferentTime])) {
+   			switchReferentPos(pointerReferentTime);
+   			pointerReferentTime++
+   		}
+	};
+	audio.play();
+} 
+
+function logKey(e) {
+	if(e.code == "Enter") {
+		nextReferent();
+	}
+}
 
 function nextReferent() {
-	if(isRecording) {
+	if(mode == "RECORDING") {
 		if(pointer < referents.length) {
 			switchReferentPos(pointer);
 			referentTime.push(audio.currentTime)
-			recordedReferentTime = referentTime;
+			recordedReferentTime = referentTime; 	// temp for testing
 			pointer++;
 		}
 	}
@@ -152,11 +116,77 @@ function switchReferentPos(position) {
 	referents[position].className ="referent-live";
 }
 
+function closePreviewPage() {
+	document.querySelector('#preview-container').style.display = "none";
+	document.querySelector('#app-container').style.display = "block";
+}
+
+/**
+ * Called when a file has been correctly selected.
+*/
+function start() {
+	// display views
+	displayViewer();
+	closePreviewPage();
+
+	// set event listener
+	document.addEventListener('keyup', logKey);
+	btn['edit'].addEventListener("click", function (e) { mode = "EDITING"; displayEditArea(); });
+	btn['cancel'].addEventListener("click", function (e) { displayViewer(); });
+	btn['record'].addEventListener("click", function (e) { if(mode != "RECORDING") { mode = "RECORDING"; isRecording = true; recordingSound(); } });
+	btn['play'].addEventListener("click", function (e) { if(mode != "PLAYING") { mode = "PLAYING"; isPlaying = true; playSound(); } });
+	btn['save'].addEventListener("click", function (e) { if(mode != "PLAYING") { displayViewer(); updateLyrics(); } });
+}
+
+/**
+ * Initiation of the application.
+*/
+function init() {
+	//mode = "EDITING";
+
+	document.querySelector("#input-file").onchange = function(e) {
+		 // verify extension
+		 var testPath = e.target.value;
+	  	if(testPath.split(".").pop() != "mp3") {
+	  		console.log("Files selected must be in mp3 format !");
+	  	}else {
+	  		fileName = e.target.value.split("\\").pop()
+	  		start();
+	  	}
+	};
+
+	document.querySelector('#btn-new').addEventListener('click', function() { document.querySelector('#input-file').click(); });
+	document.querySelector('#btn-open').addEventListener('click', function() { });
+
+}
+
+// init variables
+var mode = ""; // EDITING / PLAYING / RECORDING
 var isPlaying = false;
 var isRecording = false;
 var referentTime = [];
 var pointerReferentTime = 0;
+var recordedReferentTime = [];
+var referents;
+var pointer = 0;	// pointer for the 
 
-// init
-addLyrics();
-displayViewer();
+var lyrics = ""	// lyrics saved in raw format with carriage return (\n)
+var fileName = "";
+var audio;		// Audio element to instantiate
+
+/* DOM element */
+var view = {
+	'viewer': document.querySelector("#lyrics-viewer"),
+	'area': document.querySelector("#lyrics-textarea")
+}
+
+var btn = {
+	'edit': document.querySelector("#btn-edit"),
+	'cancel': document.querySelector("#btn-cancel"),
+	'record': document.querySelector("#btn-record"),
+	'play': document.querySelector("#btn-play"),
+	'save': document.querySelector("#btn-save")
+}
+
+
+init();
